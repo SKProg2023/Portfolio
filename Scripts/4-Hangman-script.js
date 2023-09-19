9fa35d410767f939c6ead94455f24772bcdc26ad
SKProg2023 <142337122+SKProg2023@users.noreply.github.com>
2023-09-19 11:13:32 -0400

    Update 4-Hangman-script.js

--- Scripts/4-Hangman-script.js
+++ Scripts/4-Hangman-script.js
@@ -155,7 +155,7 @@ let GametoView = function() {
     UsedField = document.createElement('p');  //creating field to show used letters
     UsedField.id ="UF";
     UsedField.innerHTML = "Used letters: ";
-    UsedField.style = `position:absolute; left: 2vw; top: 30vw;
+    UsedField.style = `position:absolute; left: 2vw; top: 33vw;
      font-size: 2vw; max-width 10vw`
     document.body.appendChild(UsedField);
 }
